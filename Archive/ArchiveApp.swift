7f810c272257c873f192c280c71dc489b0e43eda
Matthew Lu <pamalu.225@gmail.com>
2026-07-03 17:54:11 -0400

    Initial Commit

---
+++ Archive/ArchiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchiveApp.swift
+//  Archive
+//
+//  Created by Matthew Lu on 7/3/26.
+//
+
+import SwiftUI
+
+@main
+struct ArchiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
